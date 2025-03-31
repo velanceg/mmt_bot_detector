4f9862c94b7a6d81f3243312974287cc4dcaf014
Singara Velan <velanceg150795@gmail.com>
2025-03-31 23:25:31 +0530

    initial commit

---
+++ streaming_job.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType
+from bot_detector import detect_bots
+from redis_sink import write_to_redis
+from hdfs_sink import write_to_hdfs
+from ..config.config_loader import REDIS_HOST, REDIS_PORT, ACTIVITY_THRESHOLD, HDFS_PATH
+
+
+def main():
+    spark = SparkSession.builder.appName("BotUserDetector").getOrCreate()
+
+    schema = StructType([
+        StructField("user_id", StringType(), True),
+        StructField("activity_name", StringType(), True),
+        StructField("timestamp", TimestampType(), True)
+    ])
+
+    raw_events_df = (
+        spark.readStream
+        .format("csv")
+        .option("header", "true")
+        .schema(schema)
+        .load("hdfs://path_to_events")
+    )
+
+    bot_users_df = detect_bots(raw_events_df, ACTIVITY_THRESHOLD)
+
+    write_to_redis(bot_users_df, REDIS_HOST, REDIS_PORT)
+    write_to_hdfs(bot_users_df, HDFS_PATH)
+
+    spark.streams.awaitAnyTermination()
+
+
+if __name__ == "__main__":
+    main()
